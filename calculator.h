ed9bd121f06b305f00d9bb54ab8b963131073759
Fady-Latif <fady.sherif@aucegypt.edu>
2025-04-04 23:10:44 +0200

    Update calculator.h

--- calculator.h
+++ calculator.h
@@ -6,7 +6,9 @@ using namespace std;
 
 double add(double a, double b);
 double multiply(double a, double b);
-
+int subtract(int a, int b);
+float divide(int a, int b);
+int generateRandom(int min, int max);
 unsigned long long factorial(int n);
 int gcd(int a, int b);
 int lcm(int a, int b);
