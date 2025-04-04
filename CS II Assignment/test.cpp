9ddf1309e894b600ddb3e15c313a8c00af7f4882
Fady-Latif <fady.sherif@aucegypt.edu>
2025-04-04 23:16:46 +0200

    Update test.cpp

--- CS II Assignment/test.cpp
+++ CS II Assignment/test.cpp
@@ -1,19 +1,28 @@
-//
-//  test.cpp
-//  CS II Assignment
-//
-//  Created by Yahia Genena on 02/04/2025.
-//
-
-#include <iostream>
 #include "calculator.h"
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    cout << "Subtraction: 10 - 4 = " << subtract(10, 4) << endl;
-    cout << "Division: 20 / 5 = " << divide(20, 5) << endl;
-    cout << "GCD of 28 and 36 = " << gcd(28, 36) << endl;
-    cout << "Random number between 1 and 10: " << generateRandom(1, 10) << endl;
+    cout << "Basic Operations:\n";
+    cout << "3 + 5 = " << add(3, 5) << "\n";
+    cout << "10 - 4 = " << subtract(10, 4) << "\n";
+    cout << "6 * 7 = " << multiply(6, 7) << "\n";
+    cout << "20 / 4 = " << divide(20, 4) << "\n";
+
+    cout << "\nFactorial:\n";
+    cout << "5! = " << factorial(5) << "\n";
+
+    cout << "\nGCD & LCM:\n";
+    cout << "GCD(48, 18) = " << gcd(48, 18) << "\n";
+    cout << "LCM(12, 15) = " << lcm(12, 15) << "\n";
+
+    cout << "\nRandom Number:\n";
+    cout << "Random number between 1 and 100: " << generate_random(1, 100) << "\n";
+
+    cout << "\nExpression Parsing:\n";
+    const char* expr = "3+4*2-1";
+    cout << "Evaluating: " << expr << " = " << evaluate_expression(expr) << "\n";
+
     return 0;
 }
